d907ec9e0a27a476433fc19763455fead7b353ac
APCS3rd <APCS3rd@MHS-Math2-30>
2018-10-02 11:00:25 -0700

    calculate pt 5 10/02/18

---
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -0,0 +1,12 @@
+/*This class is client code that uses the Quadratic
+ * to obtain the description of a quadratic graph 
+ * @author Vivian Lin
+ * @version October 2, 2018
+ */
+public class QuadraticClient {
+	
+	public static void main(String[] args) {
+		System.out.println(Quadratic.quadrDescriber(1, 4, 4));
+	}
+
+}
